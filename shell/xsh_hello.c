b4adce52341a17e96987954df57e9ddca4bcded4
Pruthvi Raj <pkurusal@iu.edu>
2017-08-25 20:29:04 -0400

    MAde the changes for hello.c

--- shell/xsh_hello.c
+++ shell/xsh_hello.c
@@ -12,7 +12,16 @@ shellcmd xsh_hello(int nargs, char *args[]) {
 
 	int32	retval;			/* return value			*/
 	uint32	now;			/* current local time		*/
-	char	datestr[64];		/* printable date in ascii	*/
-printf("hello");
-return 0;
+	
+	if(nargs > 2){
+		printf("Too many Arguments");
+	}
+	else if(nargs < 2){
+		printf("Too few Arguments");
+	}
+	else{
+		printf("Hello %s, Welcome to the world of Xinu!!",args[1]);
+	}
+	
+	return 0;
 }
