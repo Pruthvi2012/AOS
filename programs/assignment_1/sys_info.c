34493cb431130ba64dd7d38ea7e1bbb5237d0771
Pruthvi Raj Kurusala <pkurusal@hulk.soic.indiana.edu>
2017-08-25 23:21:21 -0400

    added comments

--- programs/assignment_1/sys_info.c
+++ programs/assignment_1/sys_info.c
@@ -5,26 +5,29 @@
 
 int main(int nargs,char *args[])
 {
-    pid_t pid;
-    int p[2];
+    pid_t pid;                         /*pid for fork*/
+    int p[2];				/*p for piping*/
     char buffer[100],cmd[20];
 		
-		
+    if(nargs < 2)	{
+	printf("give atleast one proper argument");
+	return 0;
+	}	
 	pipe(p);
 	
     pid=fork();
 	    
-    if (pid==0){
+    if (pid==0){				/*Child function*/
         printf("Child PID=%d\n",getpid()); 
 		close(p[1]);
 		read(p[0], buffer, sizeof(args[1]));
 		strcat(cmd,"/bin/");
 		strcat(cmd,buffer);
-		if(strcmp(buffer,"echo")== 0){
+		if(strcmp(buffer,"echo")== 0){		/*echo functionality*/
 		execl(cmd,buffer,"HELLO WORLD",NULL);}
 		else{
 		execl(cmd,buffer,NULL);}
-    }else{
+    }else{					/*Parent function*/
         printf("Parent PID=%d\n",getpid());
 		close(p[0]);
 		write(p[1],args[1],(strlen(args[1])+1));
--- shell/xsh_hello.c
+++ shell/xsh_hello.c
@@ -1,23 +1,17 @@
-/* xsh_date.c - xsh_date */
+/* xsh_hello.c - xsh_hello */
 
 #include <xinu.h>
 #include <string.h>
 #include <stdio.h>
 
-/*------------------------------------------------------------------------
- * xsh_date - obtain and print the current month, day, year, and time
- *------------------------------------------------------------------------
- */
 shellcmd xsh_hello(int nargs, char *args[]) {
 
-	int32	retval;			/* return value			*/
-	uint32	now;			/* current local time		*/
-	
+
 	if(nargs > 2){
-		printf("Too many Arguments\n");
+		printf("Too many Arguments\n");   /*For more than 1 argument*/
 	}
 	else if(nargs < 2){
-		printf("Too few Arguments\n");
+		printf("Too few Arguments\n");	// For no arguments
 	}
 	else{
 		printf("Hello %s, Welcome to the world of Xinu!!\n",args[1]);
