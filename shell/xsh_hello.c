7b75e812dc0387459111b2f43906253c01159d83
Pruthvi Raj Kurusala <pkurusal@hulk.soic.indiana.edu>
2017-08-25 20:02:06 -0400

    Added hello script file in shell command

---
+++ shell/xsh_hello.c
@@ -0,0 +1,18 @@
+/* xsh_date.c - xsh_date */
+
+#include <xinu.h>
+#include <string.h>
+#include <stdio.h>
+
+/*------------------------------------------------------------------------
+ * xsh_date - obtain and print the current month, day, year, and time
+ *------------------------------------------------------------------------
+ */
+shellcmd xsh_hello(int nargs, char *args[]) {
+
+	int32	retval;			/* return value			*/
+	uint32	now;			/* current local time		*/
+	char	datestr[64];		/* printable date in ascii	*/
+printf("hello");
+return 0;
+}
