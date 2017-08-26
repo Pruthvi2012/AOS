ad4ae62cc3bf3a59d98352e8103719ff2e2c68c2
Pruthvi Raj Kurusala <pkurusal@hulk.soic.indiana.edu>
2017-08-25 23:06:56 -0400

    Almost Done

--- programs/assignment_1/sys_info.c
+++ programs/assignment_1/sys_info.c
@@ -1,12 +1,13 @@
-
 #include  <stdio.h>
 #include  <sys/types.h>
+#include <string.h>
+#include <unistd.h>
 
 int main(int nargs,char *args[])
 {
     pid_t pid;
     int p[2];
-	char buffer[100];
+    char buffer[100],cmd[20];
 		
 		
 	pipe(p);
@@ -17,12 +18,17 @@ int main(int nargs,char *args[])
         printf("Child PID=%d\n",getpid()); 
 		close(p[1]);
 		read(p[0], buffer, sizeof(args[1]));
-		printf("%s",buffer);
+		strcat(cmd,"/bin/");
+		strcat(cmd,buffer);
+		if(strcmp(buffer,"echo")== 0){
+		execl(cmd,buffer,"HELLO WORLD",NULL);}
+		else{
+		execl(cmd,buffer,NULL);}
     }else{
         printf("Parent PID=%d\n",getpid());
 		close(p[0]);
 		write(p[1],args[1],(strlen(args[1])+1));
-		exit(0);
+		wait(NULL);
     }
 }
 
