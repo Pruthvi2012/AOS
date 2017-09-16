1f0150b2b5ada7343750142659b5fbce33a40637
Pruthvi Raj Kurusala <pkurusal@tank.soic.indiana.edu>
2017-09-15 22:32:39 -0400

    chngs

--- include/queue.h
+++ include/queue.h
@@ -14,7 +14,7 @@
 
 struct	qentry	{		/* One per process plus two per list	*/
 	int32	qkey;		/* Key on which the queue is ordered	*/
-	pid32	processId;
+	pid32	pid;
 	//qid16	qnext;		/* Index of next process or tail	*/
 	//qid16	qprev;		/* Index of previous process or head	*/
 	struct qentry* qnext;
@@ -27,8 +27,8 @@ extern	struct qentry	queuetab[];
 
 #define	queuehead(q)	(q)
 #define	queuetail(q)	((q) + 1)
-#define	firstid(q)	(queuetab[queuehead(q)].qnext->processId)
-#define	lastid(q)	(queuetab[queuetail(q)].qprev->processId)
+#define	firstid(q)	(queuetab[queuehead(q)].qnext->pid)
+#define	lastid(q)	(queuetab[queuetail(q)].qprev->pid)
 #define	isempty(q)	(firstid(q) >= NPROC)
 #define	nonempty(q)	(firstid(q) <  NPROC)
 #define	firstkey(q)	(queuetab[firstid(q)].qkey)
